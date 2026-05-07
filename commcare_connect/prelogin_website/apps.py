70754e4ef2c0b6f572b7e5dd34732071551b4014
Jonathan Jackson <jjackson@dimagi.com>
2026-05-07 11:57:00 -0600

    feat(prelogin_website): scaffold empty app and register in LOCAL_APPS

---
+++ commcare_connect/prelogin_website/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class PreloginWebsiteConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "commcare_connect.prelogin_website"
